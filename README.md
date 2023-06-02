72885d36fdf68d98ffb301ee5f93129a06bb1544
Prasad Bhogale <prasad.bhogale@fafadiatech.com>
2023-06-02 10:45:05 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Command line Script that interacts with Open Library
+    The main purpose behind this project is to fetch details like which books was written by whom and which writter has written which book.
+# Libraries Used :
+    1.click 
+    2.requests
+# Explaination of Libraries Used:
+    -click was imported to create a command line interface for searching books in Open library.
+    - requests was imported to send HTTP requests and retrieve Json data.
+# Query used to run the project
+    python3 project1.py "Harry"
---
+++ project1.py
@@ -0,0 +1,25 @@
+import click
+import requests
+
+@click.command()
+@click.argument('query')
+def search_books(query):
+  
+    url = 'https://openlibrary.org/search.json'
+    params = {'q': query}
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    # Extract relevant book information
+    books = data.get('docs', [])
+    for book in books:
+        title = book.get('title', '')
+        author = ', '.join(book.get('author_name', []))
+        print("Title: " + title)
+        print("Author(s): " + author)
+
+        print("---")
+
+if __name__ == '__main__':
+    search_books()
+
